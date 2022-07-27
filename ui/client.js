f2c30815d797f68459f223aff8db1d390fea0e80
yilmazoncum <oncumkorkmaz@gmail.com>
2022-07-27 15:09:37 +0300

    add question logic

--- ui/client.js
+++ ui/client.js
@@ -1,2 +1,34 @@
 var io = io();
 
+/* var question = {
+    text: "what's the best programming language ?",
+    choices: ["C#", "javascript", "python", "asp.net"]
+};
+ */
+var question = {
+    text: "what's the best programming language ?",
+    choices: ["C#", "javascript", "python", "asp.net"]
+};
+//TODO: Make choice count flexible
+
+const loadQuestion = () => {
+    var choices = question.choices;
+    document.querySelector('#question').textContent = question.text;
+
+    for (var i = 0; i < choices.length; i++) {
+        var element = document.querySelector('#choice' + i);
+        element.innerHTML = choices[i];
+        guess('btn' + i, choices[i]);
+    }
+    
+}
+
+function guess(id, guess) {
+    var btn = document.getElementById(id);
+    btn.onclick = () => {
+        //TODO: save answers in array 
+        console.log("clicked " + id + guess);
+    }
+}
+
+loadQuestion();
