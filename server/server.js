73f101ea59300e6087215cc10f2aa9fe42ec85d2
yilmazoncum <oncumkorkmaz@gmail.com>
2022-07-27 14:43:03 +0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use('/',express.static('ui'));
+
+io.on('connection', (socket) => {
+    console.log(`${socket.id} has connected`);
+})
+
+http.listen(5000, () => console.log('listening on port 5000'));
