94859b1e097215c4e7412498b51ca59887d44ea2
yilmazoncum <oncumkorkmaz@gmail.com>
2022-07-28 14:29:25 +0300

    add server poll communication

--- server/server.js
+++ server/server.js
@@ -5,8 +5,24 @@ const io = require('socket.io')(http);
 
 app.use('/', express.static('ui'));
 
+var connections = []
+
 io.on('connection', (socket) => {
+    connections.push(socket)
     console.log(`${socket.id} has connected`);
+
+    socket.on('questionCreated', (question) => {
+        io.emit('showPoll',question);
+    });
+    
+    socket.on('vote', (vote) => {
+        console.log("vote server :" +vote.index + " " + vote.guess);    
+    });
+
+    socket.on('disconnect', () => {
+        connections = connections.filter((cn) => cn.id !== socket.id);
+        console.log(`${socket.id} is disconnected`);
+    })
 })
 
 http.listen(5000, () => console.log('listening on port 5000'));
--- ui/client.js
+++ ui/client.js
@@ -1,34 +1,38 @@
 var io = io();
 
-/* var question = {
-    text: "what's the best programming language ?",
-    choices: ["C#", "javascript", "python", "asp.net"]
-};
- */
-var question = {
-    text: "what's the best programming language ?",
-    choices: ["C#", "javascript", "python", "asp.net"]
-};
-//TODO: Make choice count flexible
+io.on('showPoll', (question) => {
+    loadQuestion(question)
+})
 
-const loadQuestion = () => {
+
+const loadQuestion = (question) => {
     var choices = question.choices;
     document.querySelector('#question').textContent = question.text;
 
     for (var i = 0; i < choices.length; i++) {
         var element = document.querySelector('#choice' + i);
         element.innerHTML = choices[i];
-        guess('btn' + i, choices[i]);
+        guess('btn' + i, choices[i],i);
     }
     
 }
 
-function guess(id, guess) {
+const guess = (id, guess,index) => {
     var btn = document.getElementById(id);
     btn.onclick = () => {
         //TODO: save answers in array 
         console.log("clicked " + id + guess);
-    }
+        disableBtns();
+        io.emit("vote",{guess: guess,index: index})
+    }   
 }
+
+const disableBtns = ()=>{
+    document.querySelectorAll('.btn').forEach(btn => {
+        btn.disabled = true;
+    })
+}
+
+
 
 loadQuestion();
